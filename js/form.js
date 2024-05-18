d19268d77aa174340bb2284a24492446a96af432
Tejaswiberad20 <tejaswiberad2004@gmail.com>
2024-05-18 20:51:10 +0530

    login page valid

---
+++ js/form.js
@@ -0,0 +1,19 @@
+function validateLogin() {
+    let name = document.getElementById('name').value;
+    let password = document.getElementById('password').value;
+
+    if (name === '' || password === '') {
+        alert('Please fill in all fields.');
+    } 
+     let passwordError = document.getElementById("passwordError");
+ 
+         let passwordPattern = /^\d{8}$/;
+         if (!passwordPattern.test(password)) {
+             alert("Password must be 8 digits long.");
+             return false;
+         }
+        else{
+            alert("Form Submited")
+        }
+     }
+
