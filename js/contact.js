d33518e635683b3487785ac2326ddec3db83a23a
Tejaswiberad20 <tejaswiberad2004@gmail.com>
2024-05-22 11:46:23 +0530

    rating

--- js/contact.js
+++ js/contact.js
@@ -1,67 +1,45 @@
-function star(n){
-
-			
+function star(n) {
     const star1 = document.getElementById('s1');
     const star2 = document.getElementById('s2');
     const star3 = document.getElementById('s3');
     const star4 = document.getElementById('s4');
     const star5 = document.getElementById('s5');
     const rating = document.getElementById('rating');
-    
+    const submit_btn = document.getElementById('submit_btn');
 
-    for( let i=0; i<n; i++){
-
-    if(n==1){
-        star1.src = './../Icons/star2.png';
-        star2.src = './../Icons/star1.png';
-        star3.src = './../Icons/star1.png';
-        star4.src = './../Icons/star1.png';
-        star5.src = './../Icons/star1.png';
+    if (n >= 1) {
+        star1.src = '../images/starfill.png';
+    } else {
+        star1.src = '../images/star (1).png';
     }
-
-    
-    
-    else if(n==2){
-        star1.src = './../Icons/star2.png';
-        star2.src = './../Icons/star2.png';
-        star3.src = './../Icons/star1.png';
-        star4.src = './../Icons/star1.png';
-        star5.src = './../Icons/star1.png';
+    if (n >= 2) {
+        star2.src = '../images/starfill.png';
+    } else {
+        star2.src = '../images/star (1).png';
     }
-    
-
-    else if(n==3){
-        star1.src = './../Icons/star2.png'
-        star2.src = './../Icons/star2.png'
-        star3.src = './../Icons/star2.png'
-        star4.src = './../Icons/star1.png';
-        star5.src = './../Icons/star1.png';
+    if (n >= 3) {
+        star3.src = '../images/starfill.png';
+    } else {
+        star3.src = '../images/star (1).png';
     }
-
-    else if(n==4){
-
-        star1.src = './../Icons/star2.png'
-        star2.src = './../Icons/star2.png'
-        star3.src = './../Icons/star2.png'
-        star4.src = './../Icons/star2.png'
-        star5.src = './../Icons/star1.png';
-
+    if (n >= 4) {
+        star4.src = '../images/starfill.png';
+    } else {
+        star4.src = '../images/star (1).png';
     }
-
-    else {
-
-        star1.src = './../Icons/star2.png'
-        star2.src = './../Icons/star2.png'
-        star3.src = './../Icons/star2.png'
-        star4.src = './../Icons/star2.png'
-        star5.src = './../Icons/star2.png'
-    }
-
-    rating.innerText = "Rating is: "+n+"/5"
-            
+    if (n == 5) {
+        star5.src = '../images/starfill.png';
         
+    } else {
+        star5.src = '../images/star (1).png';
     }
+
+
+    // Update rating text
+    rating.innerText = "Rating is: " + n + "/5";
 }
+
+
 function msgsending(){
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
